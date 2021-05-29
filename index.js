1956058099324db127ed53b055551ccfa75d45a6
connorsmallman <connor@remi21.eu>
2021-05-29 17:07:02 +0100

    Adds user renderer

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  res.json(Array.from(users));
+});
+
 app.use(express.static(path.resolve('./public')));
 
 const users = new Map();
